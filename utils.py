2260ff5fdeeb060cdbaa329c2f520f5cc9894fcb
dominik <dominik.barukcic@fer.hr>
2025-01-13 14:46:17 +0100

    solved task 3 - region proposal network

--- utils.py
+++ utils.py
@@ -43,35 +43,48 @@ class ConvNormActBlock(torch.nn.Sequential):
             self.append(activation_layer())
 
 
-# def decode_boxes(rel_codes, boxes, weights=(1.0, 1.0, 1.0, 1.0), bbox_xform_clip=math.log(1000.0 / 16)):
-#     """
-#     Apply predicted bounding-box regression deltas to given roi boxes.
-#     :param rel_codes: tensor with shape Nx4 containing the encoded offsets t_x, t_y, t_w, t_h
-#     :param boxes: tensor with shape Nx4 containing the anchor boxes in the format (x1, y1, x2, y2) in absolute
-#     coordinates (i.e. x in the range [0, width] and y in the range [0, height])
-#     :param weights: During training, the encoded offsets are multiplied by weights to balance the regression loss.
-#     Ignore for now :)
-#     :param bbox_xform_clip: Clipping because of the exponential function. Ignore for now :)
-#     :return: decoded bounding boxes in the format (x1, y1, x2, y2) in absolute coordinates
-#     """
-#     # Unpacking and weighting predicted transformations.
-#     wx, wy, ww, wh = weights
-#     tx = rel_codes[:, 0] / wx
-#     ty = rel_codes[:, 1] / wy
-#     tw = rel_codes[:, 2] / ww
-#     th = rel_codes[:, 3] / wh
-#     # Prevent sending too large values into torch.exp()
-#     tw = torch.clamp(tw, max=bbox_xform_clip)
-#     th = torch.clamp(th, max=bbox_xform_clip)
-#
-#
-#     boxes = boxes.to(rel_codes.dtype)
-#
-#     # YOUR CODE HERE
-#     # widths = ...
-#     # heights = ...
-#     # ctr_x = ...
-#     # ctr_y = ...
-#     # Apply transformations...
-#
-#     return decoded_boxes
+def decode_boxes(rel_codes, boxes, weights=(1.0, 1.0, 1.0, 1.0), bbox_xform_clip=math.log(1000.0 / 16)):
+    """
+    Apply predicted bounding-box regression deltas to given roi boxes.
+    :param rel_codes: tensor with shape Nx4 containing the encoded offsets t_x, t_y, t_w, t_h
+    :param boxes: tensor with shape Nx4 containing the anchor boxes in the format (x1, y1, x2, y2) in absolute
+    coordinates (i.e. x in the range [0, width] and y in the range [0, height])
+    :param weights: During training, the encoded offsets are multiplied by weights to balance the regression loss.
+    Ignore for now :)
+    :param bbox_xform_clip: Clipping because of the exponential function. Ignore for now :)
+    :return: decoded bounding boxes in the format (x1, y1, x2, y2) in absolute coordinates
+    """
+    # Unpacking and weighting predicted transformations.
+    wx, wy, ww, wh = weights
+    tx = rel_codes[:, 0] / wx
+    ty = rel_codes[:, 1] / wy
+    tw = rel_codes[:, 2] / ww
+    th = rel_codes[:, 3] / wh
+    # Prevent sending too large values into torch.exp()
+    tw = torch.clamp(tw, max=bbox_xform_clip)
+    th = torch.clamp(th, max=bbox_xform_clip)
+
+
+    boxes = boxes.to(rel_codes.dtype)
+
+    print(boxes)
+
+    # YOUR CODE HERE
+    widths = boxes[:, 2] - boxes[:, 0]
+    heights = boxes[:, 3] - boxes[:, 1]
+    ctr_x = boxes[:, 0] + 0.5 * widths
+    ctr_y = boxes[:, 1] + 0.5 * heights
+
+    # Apply transformations...
+    pred_ctr_x = ctr_x + tx * widths
+    pred_ctr_y = ctr_y + ty * heights
+    pred_w = torch.exp(tw) * widths
+    pred_h = torch.exp(th) * heights
+
+    decoded_boxes = torch.zeros_like(boxes)
+    decoded_boxes[:, 0] = pred_ctr_x - 0.5 * pred_w
+    decoded_boxes[:, 1] = pred_ctr_y - 0.5 * pred_h
+    decoded_boxes[:, 2] = pred_ctr_x + 0.5 * pred_w
+    decoded_boxes[:, 3] = pred_ctr_y + 0.5 * pred_h
+
+    return decoded_boxes
